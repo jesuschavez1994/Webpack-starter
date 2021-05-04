c92b72a98dffbd674fc1b7ec751d8ba68e6b8185
@jchavez <jesus.chavez@bizlinks.la>
2021-05-04 15:33:12 -0400

    Webpack starter

--- src/js/promise.all.js
+++
@@ -1,9 +0,0 @@
-// import { buscarHeroes } from './promesas.js'
-
-// const heroesId = 'capi';
-// const heroesId2 = 'capi';
-
-// export let Promesa = Promise.all([ buscarHeroes(heroesId), buscarHeroes(heroesId2)  ])
-//     .then( heroe => {
-//         console.log(heroe);
-// })
